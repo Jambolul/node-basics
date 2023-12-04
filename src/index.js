7f2c8d2aca4965fca467b63fc489447dad74b018
jambolul <jami.naatanen@gmail.com>
2023-12-04 15:37:30 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,96 +1,81 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { getItems, getItemsById, postItem } from "./items.js";
+import express from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+import helmet from 'helmet';
+import session from 'express-session';
+import mediaRouter from './routes/media-router.mjs';
+import userRouter from './routes/user-router.mjs';
+import authRouter from './routes/auth-router.mjs';
 import {
-  getMediaItems,
-  getMediaItemsById,
-  postMediaItem,
-  modifyMediaItem,
-  deleteMediaItem,
-} from "./media.js";
-import {
-  getAllUsers,
-  getUserById,
-  postUser,
-  modifyUser,
-  deleteUser,
-} from "./user.js";
+  errorHandler,
+  logger,
+  notFoundHandler,
+} from './middlewares/middlewares.mjs';
 
-const hostname = "127.0.0.1";
+const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
 
-app.set("view engine", "pug");
-app.set("views", "src/views");
+app.use(
+  session({
+    name: 'session',
+    keys: ['key1', 'key2'],
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      domain: 'localhost:3000',
+      path: '/',
+      expires: expiryDate,
+    },
+  })
+);
+app.set('view engine', 'pug');
+app.set('views', 'src/views');
+// Reduce Fingerprinting (security)
+app.disable('x-powered-by');
+app.set('trust proxy', 1);
+app.use(
+  session({
+    secret: 'dummy secret',
+    name: 'sessionId',
+  })
+);
+app.use(helmet());
+app.disable('x-powered-by');
 app.use(express.json());
-app.use("/docs", express.static(path.join(__dirname, "../docs")));
-app.use("/media", express.static(path.join(__dirname, "../media")));
+app.use(express.urlencoded({extended: true}));
+app.use('/docs', express.static(path.join(__dirname, '../docs')));
+// Serve uploaded mediafiles url: /media/{file}
+app.use('/media', express.static(path.join(__dirname, '../uploads')));
 
-// middleware that logs all requests and debugging
-app.use((req, res, next) => {
-  console.log("time", Date.now(), req.method, req.url);
-  next();
+// Serve uploaded mediafiles url: /media/{file}
+app.use('/forms', express.static(path.join(__dirname, '../test-forms')));
+
+// simple custom middleware for logging/debugging all requests
+if (process.env.NODE_ENV !== 'production') {
+  app.use(logger);
+}
+
+// render pug a file (home.pug) example
+app.get('/', (req, res) => {
+  const values = {title: 'Dummy REST API docs', message: 'TODO: docs'};
+  res.render('home', values);
 });
 
-app.get("/", (req, res) => {
-  const values = { title: "Dummy REST API docs", message: "TODO: docs" };
-  res.render("home", values);
-});
+// auth endpoints
+app.use('/api/auth', authRouter);
+// media endpoints
+app.use('/api/media', mediaRouter);
+// user endpoints
+app.use('/api/users', userRouter);
 
-// dummy example
-app.get("/kukkuu", (request, response) => {
-  const myResponse = { message: "no moro" };
-  //  response.json(myResponse);
-  response.sendStatus(200);
-});
-
-// other dummy pug example
-app.get("/:message", (req, res) => {
-  const values = { title: "Dummy REST API docs", message: req.params.message };
-  res.render("home", values);
-});
-
-// exmaple generic items api
-
-// get all items
-app.get("/api/items", getItems);
-// get item by id
-app.get("/api/items/:id", getItemsById);
-// modify
-app.put("/api/items");
-// add new item
-app.post("/api/items", postItem);
-// remove existing item
-app.delete("/api/items");
-
-// media functions
-
-// get all media items
-app.get("/api/media", getMediaItems);
-// get media item by id
-app.get("/api/media/:id", getMediaItemsById);
-// post media item
-app.post("/api/media/:id", postMediaItem);
-// modify media item
-app.put("/api/media/:id", modifyMediaItem);
-// delete media item
-app.delete("/api/media/:id", deleteMediaItem);
-
-// user functions
-
-// get all users
-app.get("/api/user", getAllUsers);
-// get user by id
-app.get("/api/user/:id", getUserById);
-// post user
-app.post("/api/user", postUser);
-// modify user
-app.put("/api/user/:id", modifyUser);
-// delete user
-app.delete("/api/user/:id", deleteUser);
+// All others routes => 404
+app.use(notFoundHandler);
+// default error handler
+app.use(errorHandler);
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
